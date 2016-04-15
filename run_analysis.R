1197304fe2ad42bf0a1eb64b27460de295fb23bb
numb3rcruncher52 <mwlyons52@gmail.com>
2016-04-14 22:36:25 -0400

    Initial commit

---
+++ run_analysis.R
@@ -0,0 +1,21 @@
+## run_analysis.R
+## 
+## Author: Max
+## Date: 4/14/2016
+##
+## This script downloads accelerometer data from the Samsung Galaxy S
+## smartphone and cleans it up for analysis for the Getting and Cleaning
+## data course project from Coursera
+
+## Download and load the data
+
+## Merge training and test sets to get one dataframe
+
+## Extract only mean and standard deviation for each measurement
+
+## Change activity names to be more descriptive
+
+## Relabel the variables to be descriptive
+
+## Create another tidy data set with the average of each variable for each
+## activity and subject
